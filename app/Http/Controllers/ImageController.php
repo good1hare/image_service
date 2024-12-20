0eb7f41dbe6c651ecc82762b304ea8ba9799f981
Bulat Sharifullin <good1hare@gmail.com>
2024-12-20 15:14:25 +0300

    add minio

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -12,32 +11,30 @@ class ImageController extends Controller
 {
     public function uploadImage(Request $request): JsonResponse
     {
-        return response()->json(['message' => 'Image uploaded successfully']);
+        // Проверяем наличие файла
+        if (!$request->hasFile('image') || !$request->file('image')->isValid()) {
+            return response()->json(['error' => 'Invalid image file'], 400);
+        }
 
-//        // Проверяем наличие файла
-//        if (!$request->hasFile('image') || !$request->file('image')->isValid()) {
-//            return response()->json(['error' => 'Invalid image file'], 400);
-//        }
-//
-//        // Получаем файл
-//        $image = $request->file('image');
-//
-//        // Генерируем уникальный идентификатор
-//        $identifier = Str::uuid()->toString();
-//
-//        // Создаем путь для сохранения
-//        $path = "images/{$identifier}." . $image->getClientOriginalExtension();
-//
-//        // Сохраняем файл в MinIO
-//        Storage::disk('s3')->put($path, file_get_contents($image));
-//
-//        // Возвращаем идентификатор и URL сохранённой картинки
-//        $url = Storage::disk('s3')->url($path);
-//
-//        return response()->json([
-//            'identifier' => $identifier,
-//            'url' => $url,
-//        ]);
+        // Получаем файл
+        $image = $request->file('image');
+
+        // Генерируем уникальный идентификатор
+        $identifier = Str::uuid()->toString();
+
+        // Создаем путь для сохранения
+        $path = "images/{$identifier}." . $image->getClientOriginalExtension();
+
+        // Сохраняем файл в MinIO
+        Storage::disk('s3')->put($path, file_get_contents($image));
+
+        // Возвращаем идентификатор и URL сохранённой картинки
+        $url = Storage::disk('s3')->url($path);
+
+        return response()->json([
+            'identifier' => $identifier,
+            'url' => $url,
+        ]);
     }
 }
 
--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ImageController;
-
-Route::post('/upload-image', [ImageController::class, 'uploadImage']);
-
